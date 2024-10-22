08fc22794cca50584348f7629fe5b8d191683d56
kranthikata <kranthikumarkata464@gmail.com>
2024-10-23 00:06:23 +0530

    Updated summaryRoute

--- services/getSummary.js
+++ services/getSummary.js
@@ -1,27 +1,29 @@
 const getSummary = async (request) => {
   const { startDate, endDate, category } = request.query;
+  const userId = request.user.id;
 
   let summaryQuery = `
     SELECT 
       SUM(CASE WHEN type = 'income' THEN amount ELSE 0 END) AS totalIncome,
       SUM(CASE WHEN type = 'expense' THEN amount ELSE 0 END) AS totalExpenses,
       SUM(CASE WHEN type = 'income' THEN amount ELSE -amount END) AS balance
-    FROM transactions
+    FROM transactions 
+    WHERE user_id = ?
   `;
 
-  let params = [];
+  let params = [userId];
 
   if (startDate && endDate) {
-    summaryQuery += ` WHERE date BETWEEN ? AND ?`;
+    summaryQuery += ` AND date BETWEEN ? AND ?`;
     params.push(startDate, endDate);
   }
 
   if (category) {
-    summaryQuery += params.length > 0 ? ` AND` : ` WHERE`;
-    summaryQuery += ` category_id = ?`;
+    summaryQuery += ` AND category_id = ?`;
     params.push(category);
   }
 
   return await request.db.get(summaryQuery, params);
 };
+
 module.exports = getSummary;
